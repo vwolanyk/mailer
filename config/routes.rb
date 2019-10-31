9db68779190356573df1529e185c5add18c2e2ac
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-10-30 22:54:55 -0400

    back to my roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "letter_writers#index"
   resources :letter_writers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
