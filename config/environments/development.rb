9520e2960c41762bde8b69be70c1a152017904dc
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-10-30 22:29:02 -0400

    switch to pg

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 # }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_options = {from: 'letters4november@gmail.com'}
+config.action_mailer.default_options = {from: 'letters4november@gmail               DSFJ         .com'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
